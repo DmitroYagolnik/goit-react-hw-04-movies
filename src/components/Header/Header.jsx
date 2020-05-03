a0c6c77b15bbacf8c370205fa07bc9a1020fcd8d
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-05-03 23:12:09 +0300

    fixed route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Header.module.scss';
+import routes from '../../servises/routes';
 
 const Header = () => {
   return (
@@ -8,12 +9,12 @@ const Header = () => {
       <nav className={style.nav}>
         <ul className={style.navList}>
           <li className={style.navItem}>
-            <NavLink exact to="/" className={style.navLink}>
+            <NavLink exact to={routes.HOME} className={style.navLink}>
               Home
             </NavLink>
           </li>
           <li className={style.navItem}>
-            <NavLink to="/movies" className={style.navLink}>
+            <NavLink to={routes.MOVIES} className={style.navLink}>
               Mouvies
             </NavLink>
           </li>
